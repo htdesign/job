669c7d3bec48fc9e886c93ac5a5123f0a10f6801
htdesign <h.webdesigns@gmail.com>
2021-10-14 16:43:17 +1300

    added task pop-up

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    
+  ],
+}
